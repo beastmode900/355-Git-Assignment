3f2ff8d236d398f73168070a47d3570230e44ccf
Matthew Baker <bakermj2@vcu.edu>
2023-10-24 20:23:26 -0400

    My calculator program

---
+++ Calculator.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import java.util.stream.DoubleStream;
+
+public class Calculator {
+
+    public static int add(int a, int b) {
+        return a+b;
+    }
+
+    static double multiply(int a, int b) {
+        return a*b;
+    }
+}
