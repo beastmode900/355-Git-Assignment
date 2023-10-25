940694b3a1d56f50c604fc3bbce5ac43ee9f0092
Matthew Baker <bakermj2@vcu.edu>
2023-10-24 20:24:35 -0400

    My first unit test

---
+++ CalculatorTest.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTest {
+
+    @Test
+    void add() {
+            assertEquals(4, Calculator.add(2, 2));
+    }
+}
